7289f6dcb7e32d8522630f5c038dd1b0f73db05a
Amandeep <amandeep@firmhub.ca>
2015-10-28 12:50:25 -0400

    Removed individal service setups due to redundant code

--- src/setup.js
+++ src/setup.js
@@ -3,16 +3,67 @@
 const _ = require('lodash');
 const t = require('@aso/tcomb');
 const Bluebird = require('@aso/bluebird');
+const Shell = require('shelljs');
 
-const cfg = require('../config');
+const config = require('../config');
 const dockerSetup = require('./docker');
-const kafkaSetup = require('./kafka/setup');
-// const postgresSetup = require('./postgres/setup');
 
-module.exports = dockerSetup(cfg.docker, {})
-   .then(dockerProvides => {
-      const docker = dockerProvides.docker;
-      return Bluebird.all([
-         kafkaSetup(cfg.kafka, { docker })
-      ]);
+module.exports = dockerSetup(config.docker, {})
+
+   // Create containers for all services
+   .then(provided => {
+      const services = ['zookeeper', 'kafka', 'postgres'];
+      const dockerCreate = _.partial(createContainer, provided.docker);
+      return Bluebird.reduce(services, dockerCreate, {});
+   })
+
+   .then(function done (arg) {
+      console.log( arg );
    });
+
+/////////////
+// Helpers //
+/////////////
+
+function createContainer (docker, result, name) {
+   function pullImage (cfg) {
+      return pull(cfg.containerOpts.Image);
+   }
+
+   function create (cfg) {
+      console.log(`Creating docker container "${cfg.containerName}"`);
+      return docker.createContainer(cfg.containerName, cfg.containerOpts);
+   }
+
+   return Bluebird.resolve(config[name])
+      .tap(pullImage)
+      .then(create)
+      .then(function containerCreated (info) {
+         result[name] = info.Id;
+         return result;
+      });
+}
+
+function pull (image) {
+   process.stdout.write(`Pulling "${image}"...`);
+
+   return new Bluebird(function exec (resolve, reject) {
+      const child = Shell.exec(`docker pull ${image}`, {
+         silent: true,
+         async: true
+      });
+      child.stdout.on('data', () => {
+         process.stdout.write('.');
+      });
+
+      child.stderr.on('data', (err) => {
+         console.error(err);
+         reject(err);
+      });
+
+      child.stdout.on('end', () => {
+         process.stdout.write(`done\n`);
+         resolve();
+      });
+   });
+}
