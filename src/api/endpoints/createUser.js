3f5664165fe4d466a32e30dbe127064c9d78a98d
Amandeep <amandeep@firmhub.ca>
2015-10-30 10:50:20 -0400

    Updated createUser endpoint for new model

--- src/api/endpoints/createUser.js
+++ src/api/endpoints/createUser.js
@@ -1,18 +1,29 @@
 'use strict';
-
+const _ = require('lodash');
 const joi = require('joi');
 
 module.exports = function setup (imports) {
-   const userCommands = imports['user-commands'];
+   const User = imports['user-commands'];
+   const events = imports['log-events'];
 
    return {
       inputs: {
          email: joi.string().email().required(),
-         name: joi.string().min(3).required(),
+         name: joi.string().required(),
          password: joi.string().min(8).required()
       },
       *handler (inputs) {
-         this.body = inputs;
+         // Create a user dmodel; also encrypts password
+         const user = yield User.create(inputs);
+
+         // Create a log entry
+         yield events.create({
+            actionId: 'USER_SIGNUP',
+            userId: user.id,
+            data: _.omit(user, 'id')
+         });
+
+         this.body = _.omit(user, 'password');
       }
    };
 };
