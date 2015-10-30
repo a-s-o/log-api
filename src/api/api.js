88eec6a853e8453a3a97cb9460255aad9cf0112a
Amandeep <amandeep@firmhub.ca>
2015-10-30 14:02:54 -0400

    Get all users route for debugging

--- src/api/api.js
+++ src/api/api.js
@@ -19,6 +19,14 @@ module.exports = function provider (config, imports, provide) {
    api.use(route.post('/classes/user', endpoints.createUser));
    api.use(route.put('/classes/user/:id', endpoints.updateUser));
 
+   // TODO: deactivate this route in 1.0
+   // api.use(route.get('/classes/user', function *listUsers () {
+   //    const users = imports['user-queries'];
+   //    const _ = require('lodash');
+   //    const results = yield users.findAll();
+   //    this.body = _.map(results, it => _.omit(it.toJSON(), 'password'));
+   // }));
+
    api.use(function *notFound () {
       this.throw(404, 'Not found');
    });
