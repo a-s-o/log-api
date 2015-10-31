3df00ea7609bf68787ecf76ff5326567436c4c40
Amandeep <amandeep@firmhub.ca>
2015-10-31 15:56:14 -0400

    Made kafka/operations.awaitConnection more robust

--- src/storage/kafka/kafka.js
+++ src/storage/kafka/kafka.js
@@ -30,10 +30,10 @@ const Bluebird = require('@aso/bluebird');
 const factories = require('./src/factories');
 const types = require('./src/types');
 const operations = require('./src/operations');
-const checks = require('./src/checks');
 
 const setup = Bluebird.coroutine(function *setup (config, imports) {
    const docker = imports.docker;
+   const log = imports.logger.child({ component: 'kafka' });
 
    // Start the necessary docker containers
    yield docker.startContainer( config.zookeeperContainer );
@@ -62,22 +62,12 @@ const setup = Bluebird.coroutine(function *setup (config, imports) {
    Kafka.sendMessage = operations.sendMessage;
    Kafka.createTopic = operations.createTopic;
 
-   // Constant condition is okay as the setup promise
-   // times out in the provider below
-   while (true) {  // eslint-disable-line no-constant-condition
-      try {
-         // Create client and wait for it to be ready
-         const tempClient = Kafka.createClient('log-api-init');
-         yield _.partial(checks.client.onReady, tempClient);
-         yield _.bind(tempClient.close, tempClient);
+   // Wait for client to be ready
+   log.info('Waiting (upto 20s) for Kafka to get ready');
+   yield operations.awaitConnection(Kafka, log, 20);
+   log.info('Kafka client is ready');
 
-         // Provide
-         return { kafka: Kafka };
-      } catch (ex) {
-         // Check again after 1s
-         yield Bluebird.delay(1000);
-      }
-   }
+   return { kafka: Kafka };
 
 });
 
@@ -86,11 +76,5 @@ module.exports = function provider (config, imports, provide) {
       throw new Error('config.zookeeperHost is required');
    }
 
-   const log = imports.logger.child({ component: 'kafka' });
-   log.info('Waiting (upto 10s) for Kafka to get ready');
-
-   return setup(config, imports)
-      .timeout(10000, 'Kafka client timed out [10s]')
-      .tap(() => log.info('Kafka client is ready'))
-      .nodeify(provide);
+   return setup(config, imports).nodeify(provide);
 };
--- src/storage/kafka/src/operations.js
+++ src/storage/kafka/src/operations.js
@@ -1,16 +1,65 @@
 'use strict';
 
+const _ = require('lodash');
 const t = require('@aso/tcomb');
 const Bluebird = require('@aso/bluebird');
 
 const check = require('./checks');
 const types = require('./types');
 
+// Creates a temporary client, producer and topic and waits for all
+// operations to complete before resolving. Errors occurring during the
+// timeout period will not be rejected.
+//
+// timeout is in seconds (not ms)
+
+exports.awaitConnection = Bluebird.coroutine(function *wait (Kafka, log, timeout) {
+   let clientReady = false;
+   let producerReady = false;
+   let topicCreated = false;
+   let tempClient;
+   let tempProducer;
+
+   const time = Date.now() + (timeout * 1000);
+
+   while (!clientReady || !topicCreated) {
+      if (time < Date.now()) {
+         const state = JSON.stringify({ clientReady, producerReady, topicCreated });
+         throw new Error(`Kafka client timed out [${timeout}s] ${state}`);
+      }
+
+      try {
+         if (!clientReady) {
+            // Create client and wait for it to be ready
+            tempClient = Kafka.createClient('log-api-init');
+            yield _.partial(check.client.onReady, tempClient);
+            clientReady = true;
+         }
+
+         if (!producerReady) {
+            tempProducer = Kafka.createProducer(tempClient);
+            yield _.partial(check.producer.onReady, tempProducer);
+            producerReady = true;
+         }
+
+         if (!topicCreated) {
+            yield Kafka.createTopic(tempProducer, 'test');
+            topicCreated = true;
+         }
+
+         yield _.bind(tempClient.close, tempClient);
+      } catch (ex) {
+         if (time < Date.now()) throw ex;
+         yield Bluebird.delay(1000);
+      }
+   }
+});
+
 exports.createTopic = t.typedFunc({
    inputs: [types.Producer, t.String],
    output: t.Promise,  // < Response:String >
    fn: function createTopic (producer, topicName) {
-      const created = new Bluebird(function wait (resolve, reject) {
+      return new Bluebird(function wait (resolve, reject) {
          check.producer.onReady(producer, () => {
             // Second argument is async; async=false ensures the topic
             // is created before this method returns
@@ -20,8 +69,6 @@ exports.createTopic = t.typedFunc({
             });
          });
       });
-
-      return created.timeout(2000, 'Kafka.createTopic timed out [2s]');
    }
 });
 
