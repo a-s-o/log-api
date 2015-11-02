6e55de44eadda0f33078af84354f82ca530f769b
Amandeep <amandeep@firmhub.ca>
2015-11-02 09:56:58 -0500

    Changed updateUser handler to not mutate inputs object

--- src/api/endpoints/updateUser.js
+++ src/api/endpoints/updateUser.js
@@ -12,21 +12,24 @@ module.exports = function updateUser (imports) {
          name: joi.string()
       },
       *handler (inputs, id) {
-         // Update the user document
-         const update = _.pick(inputs, 'password', 'name');
-         const user = yield User.edit(id, update);
+         // Create a copy of inputs so we are not mutating
+         // the inputs object (ex: to replace the plaintext password)
+         const request = _.clone(inputs);
+
+         // Update the user document (also encrypts plaintext password)
+         const user = yield User.edit(id, inputs);
 
          // Replace the inputs password with the encrypted version
          // so that we don't publish a plaintext password in event
-         if (inputs.password) {
-            inputs.password = user.password;
+         if (request.password) {
+            request.password = user.password;
          }
 
          // Create a log entry
          yield events.create({
             actionId: 'USER_EDIT_PROFILE',
             userId: user.id,
-            data: inputs
+            data: request
          });
 
          this.body = _.omit(user, 'password');
